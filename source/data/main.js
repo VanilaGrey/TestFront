ee5a129a851c673d3d4ad38a40512ca91942630a
Денис <Allimp_94@mail.ru>
2023-04-27 21:57:32 +0300

    начало

--- source/data/main.js
+++ source/data/main.js
@@ -6,7 +6,77 @@ export default ({ error, pageName, status }) => ({
 			status
 		}
 	},
-	description: 'My project',
-	pixelperfect: JSON.stringify({ breakpoints: [1200], ext: 'webp' }),
-	projectName: 'My app'
+	description: 'Test Front',
+	getImages(filename, additions = {}, useTablet = true, useMobile = true) {
+		const image = Object.assign(additions, {
+		  default: `images/${filename}.jpg`,
+		  default2x: `images/${filename}@2x.jpg`,
+		  webp: `images/${filename}.webp`,
+		  webp2x: `images/${filename}@2x.webp`,
+		});
+
+		if (useTablet) {
+		  image.webpTablet = `images/${filename}-tablet.webp`;
+		  image.webpTablet2x = `images/${filename}-tablet@2x.webp`;
+		}
+
+		if (useMobile) {
+		  image.webpMobile = `images/${filename}-mobile.webp`;
+		  image.webpMobile2x = `images/${filename}-mobile@2x.webp`;
+		}
+
+		return image;
+	  },
+
+	navLinks: [
+		{
+			title: 'Demos',
+			url: '#!'
+		},
+		{
+			links: [
+				{
+					title: 'Post Header',
+					url: '#!'
+				},
+				{
+					title: 'Post Layout',
+					url: '#!'
+				},
+				{
+					title: 'Share Buttons',
+					url: '#!'
+				},
+				{
+					title: 'Gallery Post',
+					url: '#!'
+				},
+				{
+					title: 'Video Post',
+					url: '#!'
+				}
+			],
+			title: 'Post',
+			url: '#!'
+		},
+		{
+			title: 'Features',
+			url: '#!'
+		},
+		{
+			title: 'Categories',
+			url: '#!'
+		},
+		{
+			title: 'Shop',
+			url: '#!'
+		},
+		{
+			title: 'Buy Now',
+			url: '#!'
+		}
+	],
+
+	pixelperfect: JSON.stringify({ breakpoints: [320, 1200], ext: 'webp' }),
+	projectName: 'Test Front',
 });
---
+++ source/data/pages/index.html.js
@@ -0,0 +1,125 @@
+export default ({ getImages }) => ({
+	pageHeading: 'Промо',
+	promoItems: [
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `Eat Right For Your Exercise Regime`,
+			mark: 'Lifestyle',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `The Look: Perfect Balance`,
+			mark: 'Lifestyle',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `Fun Things to Do in Rome`,
+			mark: 'Style',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `24 Colorful Ceilings That Add Unexpected Contrast to Any Room`,
+			mark: 'Style',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `9 New Songs to Heat Up Your Fall Playlist`,
+			mark: 'Lifestyle',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `What You Need on Your Bedside Table`,
+			mark: 'Events',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `When Two Wheels Are Better Than Four`,
+			mark: 'Travel',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `26 Living Room Ideas from the Homes of Top Designers`,
+			mark: 'Travel',
+			views: "3K Views",
+		},
+		{
+			author: 'Niek Bove',
+			datetime: {
+				date: '2018-04-08',
+				day: 8,
+				month: 'April',
+				year: 2018
+			},
+			description: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+			heading: `What Makes Your City’s Style Unique`,
+			mark: 'Music',
+			views: "3K Views",
+		},
+	].map((item, i) => ({
+		...item,
+		image: getImages(`foto-${i}`, { alt: `${item.heading}.` }, false, false)
+	}))
+});
