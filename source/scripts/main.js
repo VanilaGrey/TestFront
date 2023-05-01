b6d39396f0f3f9013dc510a22d242738bebd4d73
Денис <Allimp_94@mail.ru>
2023-05-01 21:55:33 +0300

    позиционирование

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,3 +1,5 @@
 import initPageHeader from './modules/page-header.js';
+import initScrollNav from './modules/nav.js';
 
-document.querySelectorAll('.page-header').forEach(initPageHeader);
+document.querySelectorAll('.page-header__wrapper').forEach(initPageHeader);
+document.querySelectorAll('.page-header').forEach(initScrollNav);
---
+++ source/scripts/modules/nav.js
@@ -0,0 +1,18 @@
+const POSITION = 281;
+
+const initScrollNav = () => {
+	const nav = document.querySelector('.page-header');
+	if (window.pageYOffset  < POSITION) {
+		nav.classList.add('page-header__fixed');
+		nav.classList.remove('page-header__hidden');
+		document.body.classList.add('page-header__fixed');
+	} else {
+		nav.classList.remove('page-header__fixed');
+		nav.classList.add('page-header__hidden');
+		document.body.classList.remove('page-header__fixed');
+	}
+};
+
+window.addEventListener('scroll', initScrollNav);
+
+export default initScrollNav;
